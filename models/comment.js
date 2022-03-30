d05ccb78426ee0f52947a41dd62b6dcca26ed8ea
Angela Donati <angeladonati93@gmail.com>
2022-03-30 12:05:50 -0400

    update post, user, and comment models

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Comment extends Model {}
 
-Post.init(
+Comment.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -11,13 +11,9 @@ Post.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     content: {
-      type: DataTypes.STRING(2000),
-      
+      type: DataTypes.STRING(1500),
+
     },
     date_created: {
       type: DataTypes.DATE,
@@ -32,14 +28,21 @@ Post.init(
         key: 'id',
       },
     },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    }, 
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'post',
+    modelName: 'comment',
   }
 );
 
-module.exports = Post;
+module.exports = Comment;
