ca1f6e0584002cf1e403f32cad4b31c206bec21b
Angela Donati <angeladonati93@gmail.com>
2022-03-31 12:48:29 -0400

    add dashboard render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -30,7 +30,7 @@ router.get('/', async (req, res) => {
   });
 
 // render individual post 
- router.get('/post/:id', withAuth, async (req, res) => {
+ router.get('/posts/:id', withAuth, async (req, res) => {
      try {
          const postData = await Post.findByPk(req.params.id, {
              include: [
@@ -78,6 +78,35 @@ router.get('/', async (req, res) => {
          res.json
      }
  })
+
+
+ router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password']},
+      include: [{model: Post}],
+    })
+    const user = userData.get({plain: true});
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  }catch(err){
+    res.status(500).json(err);
+  }
+ });
+
+ router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 
 module.exports = router;
