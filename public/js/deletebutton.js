4c06ef5978a276c289da078a85086db5e428d2aa
Angela Donati <angeladonati93@gmail.com>
2022-04-01 14:04:28 -0400

    add delete button to dashboard view

--- public/js/deletebutton.js
+++ public/js/deletebutton.js
@@ -2,6 +2,7 @@
     if (event.target.hasAttribute('data-id')) {
       // const id = event.target.getAttribute('data-id');
       const id = window.location.pathname.split('/')[2];
+      console.log(id)
       // console.log(id)
       // document.location.replace(`/api/posts/update/${id}`)
 
@@ -18,6 +19,32 @@
   };
   
   document
-    .querySelector('.btn-danger')
+    .getElementById('post-del')
     .addEventListener('click', delButtonHandler);
   
+    
+// dashboard del button
+    const delButtonHandlerDash = async (event) => {
+      if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+        // const id = window.location.pathname.split('/')[2];
+        console.log(id)
+        // console.log(id)
+        // document.location.replace(`/api/posts/update/${id}`)
+  
+        const response = await fetch(`/api/posts/${id}`, {
+          method: 'DELETE',
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to delete post');
+        }
+      }
+    };
+    
+    document
+      .querySelector('.del-btn-dash')
+      .addEventListener('click', delButtonHandlerDash);
+    
