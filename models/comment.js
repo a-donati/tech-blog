b8853e25f77b9c68da5bbc6b4f13a197311e31d0
Angela Donati <angeladonati93@gmail.com>
2022-04-02 10:21:37 -0400

    update comment.js

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {}
-
+// comments model
 Comment.init(
   {
     id: {
